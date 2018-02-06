68f97a10ca47c19536fdb76d7e60d2905bd34e30
Alexander Zeier <alex@hearthsim.net>
2018-02-06 22:05:30 +0100

    Add bootstrap script

---
+++ bootstrap.bat
@@ -0,0 +1,3 @@
+cd Downloader
+nuget restore
+cd ..
--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
 @echo off
 
+call bootstrap.bat
 msbuild Downloader /t:Downloader /p:Configuration=Debug
 .\Downloader\Downloader\bin\Debug\Downloader.exe .
